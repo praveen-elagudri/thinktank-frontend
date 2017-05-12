00ff35c5d6a981ab63230bac8899ff758c276e5e
Mohamad Khan <mohamadkhan19@gmail.com>
2017-05-12 16:04:34 -0700

    changes made

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -10,7 +10,7 @@
 
 angular.module('sbAdminApp')
   .controller('ChartCtrl', ['$scope', '$http' , '$timeout', function ($scope, $http, $timeout) {
-    $scope.line = {
+    /*$scope.line = {
 	    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
 	    series: ['Series A', 'Series B'],
 	    data: [
@@ -20,16 +20,45 @@ angular.module('sbAdminApp')
 	    onClick: function (points, evt) {
 	      console.log(points, evt);
 	    }
-    };
+    };*/
 
-    $scope.bar_function = function() {
+    $scope.line_function1 = function() {
         //Get sensors type
         //var data = APP_CLOUD.getHeaders(true);
         $http({
           method: 'GET',
-          url: 'https://thinktank-backend.herokuapp.com/customers'
+          url: 'https://thinktank-backend.herokuapp.com/product_prices'
         }).then(function(resp) {
-          console.log("This is response: "+resp.data.obj[1].sumofproducts);
+         // console.log("This is response: "+resp.data.obj[1].sumofproducts);
+                $scope.totalPrice = resp.data;
+                var temp1 = [ ];
+                var temp2 = [ ];
+                $scope.lables = [];
+                $scope.data = [];
+                angular.forEach($scope.totalPrice.obj,function(item){
+                   temp1.push(item.product_id);
+                   temp2.push(item.sum_product_price_ok);
+               });
+
+            
+            return  $scope.line1 = {
+                labels : temp1,
+                data : [temp2],
+                series: ['']
+            };
+        });
+
+
+    };
+
+    $scope.bar_function1 = function() {
+        //Get sensors type
+        //var data = APP_CLOUD.getHeaders(true);
+        $http({
+          method: 'GET',
+          url: 'https://thinktank-backend.herokuapp.com/sales_year'
+        }).then(function(resp) {
+         // console.log("This is response: "+resp.data.obj[1].sumofproducts);
                 $scope.totalPrice = resp.data;
                 var temp1 = [ ];
                 var temp2 = [ ];
@@ -41,10 +70,67 @@ angular.module('sbAdminApp')
                });
 
             
-            return  $scope.bar = {
+            return  $scope.bar1 = {
                 labels : temp1,
                 data : [temp2],
-                series: ['Series A']
+                series: ['']
+            };
+        });
+
+
+    };
+
+    $scope.line_function2 = function() {
+        //Get sensors type
+        //var data = APP_CLOUD.getHeaders(true);
+        $http({
+          method: 'GET',
+          url: 'https://thinktank-backend.herokuapp.com/supplier_quantity'
+        }).then(function(resp) {
+          console.log("This is response: "+resp.data.obj[1]);
+                $scope.totalPrice = resp.data;
+                var temp1 = [ ];
+                var temp2 = [ ];
+                $scope.lables = [];
+                $scope.data = [];
+                angular.forEach($scope.totalPrice.obj,function(item){
+                   temp1.push(item.supplier_id);
+                   temp2.push(item.cnt_quantity_ok);
+               });
+
+            
+            return  $scope.line2 = {
+                labels : temp1,
+                data : [temp2],
+                series: ['']
+            };
+        });
+
+
+    };
+    $scope.bar_function2 = function() {
+        //Get sensors type
+        //var data = APP_CLOUD.getHeaders(true);
+        $http({
+          method: 'GET',
+          url: 'https://thinktank-backend.herokuapp.com/order_prices'
+        }).then(function(resp) {
+          //console.log("This is response: "+resp.data.obj[1].sumofproducts);
+                $scope.totalPrice = resp.data;
+                var temp1 = [ ];
+                var temp2 = [ ];
+                $scope.lables = [];
+                $scope.data = [];
+                angular.forEach($scope.totalPrice.obj,function(item){
+                   temp1.push(item.customer_id);
+                   temp2.push(item.avg_order_price_ok);
+               });
+
+            
+            return  $scope.bar2 = {
+                labels : temp1,
+                data : [temp2],
+                series: ['']
             };
         });
 
@@ -63,7 +149,7 @@ angular.module('sbAdminApp')
         
     };*/
 
-    $scope.donut = {
+    /*$scope.donut = {
     	labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
     	data: [300, 500, 100]
     };
@@ -98,6 +184,6 @@ angular.module('sbAdminApp')
     	    'Pie' : 'PolarArea';
 		}
     };
-
+*/
     
 }]);
